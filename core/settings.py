50893a8c7e999f711f5fb70fb46151682e9b06d1
Thunderbold01 <aderitohare11@gmail.com>
2026-05-04 21:47:36 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -71,6 +71,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://pedidos-frontend-bay.vercel.app",
+    
 ]
 
 # Permitir qualquer origem do Vercel
