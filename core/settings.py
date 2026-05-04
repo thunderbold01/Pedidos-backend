ea12448ffebd40c21013d528f4b851f2256e0d78
Thunderbold01 <aderitohare11@gmail.com>
2026-05-04 21:07:29 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # Third party
     'rest_framework',
     'rest_framework_simplejwt',
-    'rest_framework.authtoken',
+    #'rest_framework.authtoken',
     'corsheaders',
 
     # Local
