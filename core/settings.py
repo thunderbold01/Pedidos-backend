0de19128308eb05a33e9dd47eeaf4234fe6314d5
Thunderbold01 <aderitohare11@gmail.com>
2026-05-05 09:54:16 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aderitohare11@gmail.com'
-EMAIL_HOST_PASSWORD = 'bolfcuhnjetaxwrx'
+EMAIL_HOST_PASSWORD = 'hinubjurgzdvkrxq'
 DEFAULT_FROM_EMAIL = 'aderitohare11@gmail.com'
 
 # ========================
