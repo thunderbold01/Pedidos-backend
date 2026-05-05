6834411af13d6e21d18256c442a616985d24ac4f
Thunderbold01 <aderitohare11@gmail.com>
2026-05-05 11:51:43 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
-# accounts/urls.py - Adicione a rota verify-2fa de volta
+# accounts/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('auth/register/', views.register_view, name='register'),
     path('auth/login/', views.login_view, name='login'),
-    path('auth/verify-2fa/', views.verify_2fa_view, name='verify_2fa'),
     path('auth/logout/', views.logout_view, name='logout'),
     path('user/me/', views.user_me, name='user_me'),
 ]
